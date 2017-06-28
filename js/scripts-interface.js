56e1f679024d97e93ecd6281763112342290a72e
Brian Rush and Nick Wise <brian+nick@>
2017-06-28 13:48:12 -0700

    split the word into individual letters (but not display it yet)

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -12,10 +12,21 @@ $(document).ready(function() {
   // var exampleInstance = new ExampleModule('args');
   $("#start-button").click(function() {
 
-    var dinoName = $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1', function(responseJSON){
-      console.log(responseJSON);      
+    var dinoName = $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1').then(function(responseJSON){
+      console.log(responseJSON);
+      var toBeSplit = responseJSON[0][0];
+      console.log(toBeSplit);
+      // var dinoLetters = [];
+      var splitDinoName = toBeSplit.split("");
+      console.log(splitDinoName);
+      // console.log(typeof(responseJSON[0]));
+      // dinoLetters.push(splitDinoName);
+      // console.log(dinoLetters);
+    }).fail(function(error){
+      $('#the-word').text("Sorry! Something's not working. :(")
     });
-    console.log(dinoName);
+    // console.log(dinoName);
+    // console.log(dinoName);
   });
 
 
