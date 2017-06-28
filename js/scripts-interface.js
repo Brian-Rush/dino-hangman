6409b9c7b9c138b11d803397747e22f705a5a2d4
Brian Rush and Nick Wise <brian+nick@>
2017-06-28 14:18:33 -0700

    split the generated dinosaur word into individual capital letters (next, need to write comparison function)

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -12,21 +12,23 @@ $(document).ready(function() {
   // var exampleInstance = new ExampleModule('args');
   $("#start-button").click(function() {
 
-    var dinoName = $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1').then(function(responseJSON){
+     $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1').then(function(responseJSON){
       console.log(responseJSON);
       var toBeSplit = responseJSON[0][0];
       console.log(toBeSplit);
-      // var dinoLetters = [];
-      var splitDinoName = toBeSplit.split("");
+      var splitDinoName = toBeSplit.toUpperCase().split("");
       console.log(splitDinoName);
-      // console.log(typeof(responseJSON[0]));
-      // dinoLetters.push(splitDinoName);
-      // console.log(dinoLetters);
+      for (i=0; i < splitDinoName.length; i++) {
+        $('#the-word').append("<div class='letter'><span value=" + splitDinoName[i] + ">" + splitDinoName[i] + "</span></div>")
+      }
     }).fail(function(error){
       $('#the-word').text("Sorry! Something's not working. :(")
     });
-    // console.log(dinoName);
-    // console.log(dinoName);
+
+    // $('.letter-button').on(click, function() {
+    //   var clickedLetter = this.val()
+    // })
+
   });
 
 
