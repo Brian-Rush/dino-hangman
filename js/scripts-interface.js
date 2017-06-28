7d7eb662e201661351f94cc258729bce6fc89014
Brian Rush and Nick Wise <brian+nick@>
2017-06-28 16:44:36 -0700

    display answer and congratulations message when user types in correct answer and submits, regardless of typecase (still need to clear the fields when new game is started)

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -11,19 +11,22 @@
 $(document).ready(function() {
 
   var splitDinoName = [];
+  var toBeSplit = "";
   // var exampleInstance = new ExampleModule('args');
   $("#start-button").click(function() {
 
      $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1').then(function(responseJSON){
       console.log(responseJSON);
-      var toBeSplit = responseJSON[0][0];
-      console.log(toBeSplit);
+      toBeSplit = responseJSON[0][0];
+      wordUpper = toBeSplit.toUpperCase();
+      console.log(wordUpper);
       splitDinoName = toBeSplit.toUpperCase().split("");
       var numberOfLetters = splitDinoName.length;
       console.log(splitDinoName);
       // for (i=0; i < splitDinoName.length; i++) {
       //   $('#the-word').append("<div class='letter'></div>")
       // }
+      $('#the-word').text("");
       for (i=0; i < splitDinoName.length; i++) {
         $('#the-word').append("<div class='letter'><span class=" + splitDinoName[i] + ">" + splitDinoName[i] + "</span></div>")
       }
@@ -42,6 +45,17 @@ $(document).ready(function() {
       $('#the-word span.' + clickedLetter).css('visibility', 'visible');
     }
   })
+
+
+  $('.guess').submit(function(event) {
+    event.preventDefault();
+    var guessedWord = $('#guess').val().toUpperCase();
+    console.log(guessedWord);
+    if (guessedWord == wordUpper) {
+      $('span').css('visibility', 'visible');
+      $('.guess').after("Congrats! You saved a dinosaur from some sicko who wants to hang dinosaurs if random people can't guess words!")
+    };
+  });
 
   // console.log(exampleInstance.examplePrototype());
 });
